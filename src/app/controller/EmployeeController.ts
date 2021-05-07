017278b6944b0b734411172a890a74a5af06b7f9
filipe-mariz <57136731+filipe-mariz@users.noreply.github.com>
2021-05-07 14:45:01 -0300

    feat: crud employee was crieated

---
+++ src/app/controller/EmployeeController.ts
@@ -0,0 +1,82 @@
+import { Request, Response } from 'express';
+import { getRepository } from 'typeorm';
+import Employee from '../model/Employee';
+import employeeView from '../view/EmployeeView';
+import * as Yup from 'yup'
+
+export default {
+  async create(request: Request, response: Response) {
+    const {
+      name,
+      level_authorization,
+      user_name,
+      password,
+      passwordConfirmation
+    } = request.body;
+
+    const employeeRepository = await getRepository(Employee);
+    
+    const userExist = await employeeRepository.findOne({ where: { user_name }})
+    if (userExist) {
+      return response.status(402).json({ 
+        message: "This user name is in use",
+        solution: "Chose other user name" 
+      })
+    }
+    if(password != passwordConfirmation) {
+      return response.status(402).json({ 
+        message: "Passwords do not match",
+        solution: "Put exactly the same password in the confirmation" 
+      })
+    }
+
+    const data = {
+      name,
+      level_authorization,
+      user_name,
+      password,
+      passwordConfirmation
+    }
+
+    const schemma = Yup.object().shape({
+      name: Yup.string().required(),
+      level_authorization: Yup.number().required(),
+      user_name: Yup.string().required(),
+      password: Yup.string().required(),
+      passwordConfirmation: Yup.string().required(),
+    })
+
+    await schemma.validate(data, {
+      abortEarly: false
+    })
+
+    const employee = employeeRepository.create(data);
+    await employeeRepository.save(employee);
+
+    return response.status(200).json({
+      message: "employee created"
+    })
+  },
+
+  async viewOne(request: Request, response: Response) {
+    const { id } = request.params
+
+    const employee = await getRepository(Employee).findOneOrFail(id);
+
+    return response.json(employeeView.Render(employee))
+  },
+
+  async viewAll(request: Request, response: Response) {
+    const employeeRepository = getRepository(Employee);
+
+    const employee = await employeeRepository.find();
+
+    return response.json(employeeView.renderMany(employee))
+  },
+
+  async destroy(request: Request, response: Response) {
+    const results = await getRepository(Employee).delete(request.params.id)
+
+    return response.json({ message: 'User deleted'});
+  }
+}
