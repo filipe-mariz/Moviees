eb3695aa779fc2c681445fc39d26960130a62716
filipe-mariz <57136731+filipe-mariz@users.noreply.github.com>
2021-05-07 14:45:18 -0300

    feat: connections was crieated

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,3 +1,11 @@
-import { createConnection } from 'typeorm';
+import { Connection, createConnection, getConnectionOptions } from "typeorm";
 
-createConnection();
+export default async (): Promise<Connection> => {
+    const dafaultOptions = await getConnectionOptions();
+
+    return createConnection(
+        Object.assign(dafaultOptions, {
+            database: process.env.NODE_ENV === 'Test' ? "database_test" : dafaultOptions.database
+        })
+    )
+}
