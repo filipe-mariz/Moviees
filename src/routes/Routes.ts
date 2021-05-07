b0254f42d41e29be864c8609b1553bb8763fafbd
filipe-mariz <57136731+filipe-mariz@users.noreply.github.com>
2021-05-07 14:45:38 -0300

    feat: routes was crieated

--- src/routes/Routes.ts
+++ src/routes/Routes.ts
@@ -1,5 +1,8 @@
 import { Router } from 'express';
+import EmployeeController from '../app/controller/EmployeeController';
 
 const routes = Router();
+
+routes.post('/create-employee', EmployeeController.create);
 
 export default routes
