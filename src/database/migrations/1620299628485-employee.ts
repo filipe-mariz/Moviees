385e393c80f182d1673d99f6f602f168b76f0fd4
filipe-mariz <57136731+filipe-mariz@users.noreply.github.com>
2021-05-07 12:08:08 -0300

    feat: employee migrations was crieated

---
+++ src/database/migrations/1620299628485-employee.ts
@@ -0,0 +1,37 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+import { Table } from "typeorm/schema-builder/table/Table";
+
+export class employee1620399628485 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(new Table({
+      name: 'employee',
+      columns: [
+        {
+          name: 'id',
+          type: 'integer',
+          isPrimary: true,
+          unsigned: true,
+          isGenerated: true,
+          generationStrategy: 'increment'
+        },
+
+        {
+          name: 'name',
+          type: 'varchar',
+          isNullable: false
+        },
+
+        {
+          name: 'level_authorization',
+          type: 'integer',
+          isNullable: false
+        }
+      ]
+    }))
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('employee');
+  }
+
+}
