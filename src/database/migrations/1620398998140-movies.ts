cb4dccfffb0db7f92e961e66ca7fdd93e576c7c6
filipe-mariz <57136731+filipe-mariz@users.noreply.github.com>
2021-05-07 12:08:18 -0300

    feat: movies migrations was crieated

--- src/database/migrations/1620398998140-movies.ts
+++ src/database/migrations/1620398998140-movies.ts
@@ -1,11 +1,60 @@
 import {MigrationInterface, QueryRunner} from "typeorm";
+import { Table } from "typeorm/schema-builder/table/Table";
+import { date } from "yup/lib/locale";
 
 export class movies1620398998140 implements MigrationInterface {
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(new Table({
+      name: 'movies',
+      columns: [
+        {
+          name: 'id',
+          type: 'integer',
+          isPrimary: true,
+          unsigned: true,
+          isGenerated: true,
+          generationStrategy: 'increment'
+        },
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-    }
+        {
+          name: 'name',
+          type: 'varchar',
+          isNullable: false
+        },
 
+        {
+          name: 'time',
+          type: 'varchar',
+          isNullable: false
+        },
+
+        {
+          name: 'price',
+          type: 'float',
+          isNullable: false
+        },
+
+        {
+          name: 'description',
+          type: 'varchar',
+          isNullable: false
+        },
+
+        {
+          name: 'data_born',
+          type: 'date',          
+        },
+
+        {
+          name: 'data_end',
+          type: 'date'
+        }
+      ]
+    }))
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('movies')
+  }
 }
