d0b7d9cbbde7dab2326db8f4f6deb37f7ccba413
filipe-mariz <57136731+filipe-mariz@users.noreply.github.com>
2021-05-07 11:50:32 -0300

    feat: new project

---
+++ src/database/migrations/1620398998140-movies.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class movies1620398998140 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
