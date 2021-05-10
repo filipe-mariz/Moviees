1e7ed45c46d39cf2458ca14dd0a8c048a2d0da4c
filipe-mariz <57136731+filipe-mariz@users.noreply.github.com>
2021-05-10 16:46:30 -0300

    feat: register new middlewares

--- src/routes/Employ.Routes.ts
+++ src/routes/Employ.Routes.ts
@@ -2,13 +2,14 @@ import { Router } from 'express';
 import EmployeeController from '../app/controller/EmployeeController';
 import LoginController from '../app/controller/LoginController';
 import authMiddleware from '../app/middleware/AuthMiddleware';
+import bossMiddleware from '../app/middleware/BossMiddleware';
 
 const employ = Router();
 
 employ.post('/employee/login', LoginController.login);
-employ.post('/employee', EmployeeController.create);
+employ.post('/employee', bossMiddleware, EmployeeController.create);
 employ.get('/employee', authMiddleware, EmployeeController.viewAll);
 employ.get('/employee/:id', authMiddleware, EmployeeController.viewOne);
-employ.delete('/employee/:id', authMiddleware, EmployeeController.destroy);
+employ.delete('/employee/:id', bossMiddleware, EmployeeController.destroy);
 
 export default employ;
--- src/routes/Movies.Routes.ts
+++ src/routes/Movies.Routes.ts
@@ -2,12 +2,13 @@ import { Router } from 'express';
 import MoviesController from '../app/controller/MoviesController';
 import authMiddleware from '../app/middleware/AuthMiddleware'; 
 import validationMiddleware from '../app/middleware/ValidationMiddleware';
+import bossMiddleware from '../app/middleware/BossMiddleware';
 
 const movies = Router();
 
 movies.post('/movies', authMiddleware, validationMiddleware, MoviesController.create);
 movies.get('/movies', authMiddleware, MoviesController.viewAll);
 movies.get('/movies/:id', authMiddleware, MoviesController.viewOne);
-movies.delete('/movies/:id', authMiddleware, MoviesController.destroy);
+movies.delete('/movies/:id', authMiddleware, bossMiddleware, MoviesController.destroy);
 
 export default movies;
