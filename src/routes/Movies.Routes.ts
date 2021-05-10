27919b632f20d351bad1af29436c344ac4994436
filipe-mariz <57136731+filipe-mariz@users.noreply.github.com>
2021-05-10 16:23:19 -0300

    feat: new routes was register

--- src/routes/Movies.Routes.ts
+++ src/routes/Movies.Routes.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import MoviesController from '../app/controller/MoviesController';
 import authMiddleware from '../app/middleware/AuthMiddleware'; 
+import validationMiddleware from '../app/middleware/ValidationMiddleware';
 
 const movies = Router();
 
-movies.post('/movies', authMiddleware, MoviesController.create);
+movies.post('/movies', authMiddleware, validationMiddleware, MoviesController.create);
 movies.get('/movies', authMiddleware, MoviesController.viewAll);
 movies.get('/movies/:id', authMiddleware, MoviesController.viewOne);
 movies.delete('/movies/:id', authMiddleware, MoviesController.destroy);
