692e100f23b8f4f2d99c568e700bdc7160b4b1c6
filipe-mariz <57136731+filipe-mariz@users.noreply.github.com>
2021-05-10 17:45:11 -0300

    feat: new routes was crieated

--- src/routes/Movies.Routes.ts
+++ src/routes/Movies.Routes.ts
@@ -1,12 +1,15 @@
 import { Router } from 'express';
+import multer from 'multer';
 import MoviesController from '../app/controller/MoviesController';
 import authMiddleware from '../app/middleware/AuthMiddleware'; 
 import validationMiddleware from '../app/middleware/ValidationMiddleware';
 import bossMiddleware from '../app/middleware/BossMiddleware';
+import multerConfig from '../config/multer';
 
 const movies = Router();
+const upload = multer(multerConfig);
 
-movies.post('/movies', authMiddleware, validationMiddleware, MoviesController.create);
+movies.post('/movies', authMiddleware, validationMiddleware, upload.array('image'),  MoviesController.create);
 movies.get('/movies', authMiddleware, MoviesController.viewAll);
 movies.get('/movies/:id', authMiddleware, MoviesController.viewOne);
 movies.delete('/movies/:id', authMiddleware, bossMiddleware, MoviesController.destroy);
